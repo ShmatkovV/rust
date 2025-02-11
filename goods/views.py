afa4d1154d9ebbe115ff5d493fa25a3cc30ede9a
ShmatkovV <shmatkovvvav@gmail.com>
2025-02-11 21:57:02 +0300

    urls goods

--- goods/views.py
+++ goods/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 from goods.models import Products
 
 
@@ -7,12 +8,19 @@ def catalog(request):
 
     goods = Products.objects.all()
 
+    context = {
+        "title": "Home - Каталог",
+        "goods": goods,
+    }
+    return render(request, "goods/catalog.html", context)
+
+
+def product(request, product_id):
+
+    product = Products.objects.get(id=product_id)
 
     context = {
-        'title': 'Home - Каталог',
-        'goods': goods,
-    }
-    return render(request, 'goods/catalog.html', context)
+        "product": product
+        }
 
-def product(request):
-    return render(request, 'goods/product.html')
+    return render(request, "goods/product.html", context=context)
